c33df550a7e1c1733688b6762c33555702632fb8
Filip Strajnar <filip.strajnar@gmail.com>
2022-01-05 21:52:34 +0100

    author

--- toml_adapt/main.py
+++ toml_adapt/main.py
@@ -17,19 +17,19 @@
 from DocumentOperations import DocumentOperationsEnum
 from ManipulateToml import DoOperation
 
-operation: DocumentOperationsEnum=DocumentOperationsEnum.ADD
+operation: DocumentOperationsEnum=DocumentOperationsEnum.REMOVE
 
 DoOperation(operation,
-            toml_file_path="Examples/Cargo.toml",
+            toml_file_path="examples/Cargo.toml",
             dependency_name="testing777",
             dependency_version="1.1.1")
 
 DoOperation(operation,
-            toml_file_path="Examples/poetry_pyproject.toml",
+            toml_file_path="examples/poetry_pyproject.toml",
             dependency_name="testing777",
             dependency_version="1.1.1")
 
 DoOperation(operation,
-            toml_file_path="Examples/flit_pyproject.toml",
+            toml_file_path="examples/flit_pyproject.toml",
             dependency_name="testing777",
             dependency_version="1.1.1")
