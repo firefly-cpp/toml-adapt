44cda7bce5b3d87f813541e39485ee9a23bb9758
Filip Strajnar <filip.strajnar@gmail.com>
2022-01-06 21:21:20 +0100

    Removed deprecated parts of code

--- toml_adapt/ManipulateToml.py
+++ toml_adapt/ManipulateToml.py
@@ -1,15 +1,9 @@
-from typing import Any, MutableMapping
-from Common import TomlBaseManipulation
 from DocumentOperations import DocumentOperationsEnum
 from DocumentType import DocumentTypeEnum
 from Match import FindMatch
 from AllShapes import all_shapes
 from Shape import Shape
 from ToolExtraction.ToolExtraction import IsFileCargo, IsFileFlit, IsFileJuliaPkg, IsFilePoetry
-from FlitActions import AddFlitDependency, RemoveFlitDependency
-from PoetryActions import AddPoetryDependency, AddPoetryDevDependency, RemovePoetryDevDependency
-from CargoActions import AddCargoDevDependency, RemoveCargoDependency, RemoveCargoDevDependency
-from PoetryActions import RemovePoetryDependency
 
 def ExtractTool(toml_file_path: str) -> DocumentTypeEnum:    
     options_list=[
@@ -21,66 +15,6 @@ def ExtractTool(toml_file_path: str) -> DocumentTypeEnum:
         return FindMatch(options_list,lambda check: check)
     except:
         raise Exception("Invalid document type.")
-
-
-def AddCargoDependency(toml_file_path:str,
-                        dependency_name: str,
-                        dependency_version:str):
-    def cargo(toml_full_dict: MutableMapping[str,Any]):
-        toml_full_dict["dependencies"][dependency_name]=f"{dependency_version}"
-    TomlBaseManipulation(toml_file_path,cargo)
-
-
-    
-def AddDependency(toml_file_path: str,
-                  dependency_name: str,
-                  dependency_version:str) -> None:
-    tool_used=ExtractTool(toml_file_path)
-    if(tool_used.POETRY):
-        AddPoetryDependency(toml_file_path,dependency_name,dependency_version)
-    if(tool_used.FLIT):
-        AddFlitDependency(toml_file_path,dependency_name,dependency_version)
-    if(tool_used.CARGO):
-        AddCargoDependency(toml_file_path,dependency_name,dependency_version)
-    if(tool_used.JULIAPKG):
-        pass # Probably not possible
-    
-def RemoveDependency(toml_file_path: str,
-                  dependency_name: str):
-    tool_used=ExtractTool(toml_file_path)
-    if(tool_used.POETRY):
-        RemovePoetryDependency(toml_file_path,dependency_name)
-    if(tool_used.FLIT):
-        RemoveFlitDependency(toml_file_path,dependency_name)
-    if(tool_used.CARGO):
-        RemoveCargoDependency(toml_file_path,dependency_name)
-    if(tool_used.JULIAPKG):
-        pass # Probably not possible
-
-def AddDevDependency(toml_file_path: str,
-                  dependency_name: str,
-                  dependency_version:str) -> None:
-    tool_used=ExtractTool(toml_file_path)
-    if(tool_used.POETRY):
-        AddPoetryDevDependency(toml_file_path,dependency_name,dependency_version)
-    if(tool_used.FLIT):
-        AddFlitDependency(toml_file_path,dependency_name,dependency_version)
-    if(tool_used.CARGO):
-        AddCargoDevDependency(toml_file_path,dependency_name,dependency_version)
-    if(tool_used.JULIAPKG):
-        pass # Probably not possible
-    
-def RemoveDevDependency(toml_file_path: str,
-                  dependency_name: str):
-    tool_used=ExtractTool(toml_file_path)
-    if(tool_used.POETRY):
-        RemovePoetryDevDependency(toml_file_path,dependency_name)
-    if(tool_used.FLIT):
-        RemoveFlitDependency(toml_file_path,dependency_name)
-    if(tool_used.CARGO):
-        RemoveCargoDevDependency(toml_file_path,dependency_name)
-    if(tool_used.JULIAPKG):
-        pass # Probably not possible
 
 def GetMatchingShape(toml_file_path:str):
     for shape in all_shapes:
--- toml_adapt/main.py
+++ toml_adapt/main.py
@@ -1,19 +1,3 @@
-#AddDependency("Examples/poetry_pyproject.toml","automatic","9.9.9")
-#AddDependency("Examples/Cargo.toml","automatic","9.9.9")
-#AddDependency("Examples/flit_pyproject.toml","automatic","9.9.9")
-
-#AddDevDependency("Examples/poetry_pyproject.toml","automatic-dev","9.9.9")
-#AddDevDependency("Examples/Cargo.toml","automatic-dev","9.9.9")
-#AddDevDependency("Examples/flit_pyproject.toml","automatic-dev","9.9.9")
-
-#RemoveDependency("Examples/poetry_pyproject.toml","automatic")
-#RemoveDependency("Examples/Cargo.toml","automatic")
-#RemoveDependency("Examples/flit_pyproject.toml","automatic")
-
-#RemoveDevDependency("Examples/poetry_pyproject.toml","automatic-dev")
-#RemoveDevDependency("Examples/Cargo.toml","automatic-dev")
-#RemoveDevDependency("Examples/flit_pyproject.toml","automatic-dev")
-
 from DocumentOperations import DocumentOperationsEnum
 from ManipulateToml import DoOperation
 
